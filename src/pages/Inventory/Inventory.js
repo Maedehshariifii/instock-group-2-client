423ee56f7a045946af36ce447a3dfcb91cd045b8
Ryan Nguyen <ryan.ngn@outlook.com>
2024-03-20 00:13:53 -0400

    complete header responsive

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,6 +1,9 @@
+import Header from "../../components/Header/Header";
+
 function Inventory() {
   return (
     <div>
+      <Header activeNavItem='inventory' />
       <h2>Inventory</h2>
       {/* Placeholder content for inventory page */}
     </div>
---
+++ src/pages/UnmatchedRoutes/UnmatchedRoutes.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header";
+
+export default function UnmatchedRoutes() {
+    return (
+        <>
+            <Header />
+            <div>
+                <h1>404 - Page Not Found</h1>
+                <p>The page you are looking for does not exist.</p>
+            </div>
+        </>
+    );
+}
+
