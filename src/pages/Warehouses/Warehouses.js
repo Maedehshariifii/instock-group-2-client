158f2a7f99d09a6af95a0d381e93a21a4c5a2056
Ryan Nguyen <ryan.ngn@outlook.com>
2024-03-21 19:54:25 -0400

    create footer component

--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 function Warehouses() {
@@ -6,6 +7,7 @@ function Warehouses() {
       <Header activeNavItem='warehouses' />
       <h2>Welcome to Warehouses</h2>
       {/* Placeholder content for warehouses page */}
+      <Footer />
     </div>
   );
 }
