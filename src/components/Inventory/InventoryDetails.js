648b7d7ecf2c70a8651785f8cd6418f09649f800
Falak Zahra <falak.zahra16@gmail.com>
2024-03-25 22:04:42 -0600

    Add styling for the header part of the inventory details card

--- src/components/Inventory/InventoryDetails.js
+++ src/components/Inventory/InventoryDetails.js
@@ -30,52 +30,58 @@ const InventoryDetails = () => {
       }
     };
     fetchInventoryDetails();
-  }, []);
+  });
 
   return (
     <>
       {detailsLoaded ? (
-        <section className="card-container">
-          <h1 className="card-container__heading">
-            <img
-              src={BackIcon}
-              alt="Back Icon"
-              className="inventory-card__icon"
-            />
-            {inventoryDetails.item_name}
-          </h1>
-          <button className="edit-cta">
-            <img
-              src={EditIcon}
-              alt="Edit Icon"
-              className="inventory-card__icon"
-            />
-          </button>
+        <div className="card-container">
+          <div className="inventory-details__card-header">
+            <h1 className="card-container__heading">
+              <img
+                src={BackIcon}
+                alt="Back Icon"
+                className="inventory-card__icon"
+              />
+              {inventoryDetails.item_name}
+            </h1>
+            <button className="edit-cta">
+              <img
+                src={EditIcon}
+                alt="Edit Icon"
+                className="inventory-card__icon--details"
+              />
+            </button>
+          </div>
           <hr className="inventory-card__divider"></hr>
-          <h4 className="inventory-card__heading--mobile">ITEM DESCRIPTION:</h4>
-          <p className="inventory-card__item-details">
-            {inventoryDetails.description}
-          </p>
-          <h4 className="inventory-card__heading--mobile">CATEGORY:</h4>
-          <p className="inventory-card__item-details">
-            {inventoryDetails.category}
-          </p>
-          <h4 className="inventory-card__heading--mobile">STATUS</h4>
-          <p
-            className={`inventory-card__status inventory-card__status--${statusClass}`}
-          >
-            {inventoryDetails.status.toUpperCase()}
-          </p>
+          <div className="inventory-details__card">
+            <h4 className="inventory-card__heading--mobile">
+              ITEM DESCRIPTION:
+            </h4>
+            <p className="inventory-card__item-details">
+              {inventoryDetails.description}
+            </p>
+            <h4 className="inventory-card__heading--mobile">CATEGORY:</h4>
+            <p className="inventory-card__item-details">
+              {inventoryDetails.category}
+            </p>
+            <h4 className="inventory-card__heading--mobile">STATUS</h4>
+            <p
+              className={`inventory-card__status inventory-card__status--${statusClass}`}
+            >
+              {inventoryDetails.status.toUpperCase()}
+            </p>
 
-          <h4 className="inventory-card__heading--mobile">QTY</h4>
-          <p className="inventory-card__item-details">
-            {inventoryDetails.quantity}
-          </p>
-          <h4 className="inventory-card__heading--mobile">WAREHOUSE</h4>
-          <p className="inventory-card__item-details">
-            {inventoryDetails.warehouse_name}
-          </p>
-        </section>
+            <h4 className="inventory-card__heading--mobile">QTY</h4>
+            <p className="inventory-card__item-details">
+              {inventoryDetails.quantity}
+            </p>
+            <h4 className="inventory-card__heading--mobile">WAREHOUSE</h4>
+            <p className="inventory-card__item-details">
+              {inventoryDetails.warehouse_name}
+            </p>
+          </div>
+        </div>
       ) : (
         "Loading"
       )}
