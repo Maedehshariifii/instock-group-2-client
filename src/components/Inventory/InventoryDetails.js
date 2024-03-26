047b0e7738c0af40b67d871bd601d9293a8cd9f8
Falak Zahra <falak.zahra16@gmail.com>
2024-03-25 20:55:14 -0600

    Fetch data for a specific inventory item from the backend, and render the details

--- src/components/Inventory/InventoryDetails.js
+++ src/components/Inventory/InventoryDetails.js
@@ -11,6 +11,11 @@ const InventoryDetails = () => {
   const { id } = useParams();
   const [inventoryDetails, setInventoryDetails] = useState([]);
 
+  const statusClass = inventoryDetails.status
+    .split(" ")
+    .join("-")
+    .toLowerCase();
+
   // This useEffect fetches the inventory data when the component mounts
   useEffect(() => {
     const fetchInventoryDetails = async () => {
@@ -45,6 +50,30 @@ const InventoryDetails = () => {
             className="inventory-card__icon"
           />
         </button>
+        <hr className="inventory-card__divider"></hr>
+        <h4 className="inventory-card__heading--mobile">ITEM DESCRIPTION:</h4>
+        <p className="inventory-card__item-details">
+          {inventoryDetails.description}
+        </p>
+        <h4 className="inventory-card__heading--mobile">CATEGORY:</h4>
+        <p className="inventory-card__item-details">
+          {inventoryDetails.category}
+        </p>
+        <h4 className="inventory-card__heading--mobile">STATUS</h4>
+        <p
+          className={`inventory-card__status inventory-card__status--${statusClass}`}
+        >
+          {inventoryDetails.status.toUpperCase()}
+        </p>
+
+        <h4 className="inventory-card__heading--mobile">QTY</h4>
+        <p className="inventory-card__item-details">
+          {inventoryDetails.quantity}
+        </p>
+        <h4 className="inventory-card__heading--mobile">WAREHOUSE</h4>
+        <p className="inventory-card__item-details">
+          {inventoryDetails.warehouse_name}
+        </p>
       </section>
     </>
   );
