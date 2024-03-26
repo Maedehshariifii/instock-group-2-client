7ce35417cbd443b35becdc1459160859a60247b3
Falak Zahra <falak.zahra16@gmail.com>
2024-03-25 21:36:25 -0600

    Add a loading state to ensure data is fetched before rendering

--- src/components/Inventory/InventoryDetails.js
+++ src/components/Inventory/InventoryDetails.js
@@ -9,12 +9,12 @@ import "./InventoryDetails.scss";
 
 const InventoryDetails = () => {
   const { id } = useParams();
-  const [inventoryDetails, setInventoryDetails] = useState([]);
+  const [inventoryDetails, setInventoryDetails] = useState({});
+  const [detailsLoaded, setDetailsLoaded] = useState(false);
 
   const statusClass = inventoryDetails.status
-    .split(" ")
-    .join("-")
-    .toLowerCase();
+    ? inventoryDetails.status.split(" ").join("-").toLowerCase()
+    : ""; // Safely compute statusClass only after ensuring the data exists
 
   // This useEffect fetches the inventory data when the component mounts
   useEffect(() => {
@@ -23,58 +23,62 @@ const InventoryDetails = () => {
         const resp = await axios.get(
           `http://localhost:8080/api/inventories/${id}`
         );
-        console.log(resp.data);
         setInventoryDetails(resp.data);
+        setDetailsLoaded(true);
       } catch (error) {
         console.error("Error fetching inventory data:", error);
       }
     };
     fetchInventoryDetails();
-  }, [id]);
+  }, []);
 
   return (
     <>
-      <section className="card-container">
-        <h1 className="card-container__heading">
-          <img
-            src={BackIcon}
-            alt="Sort Icon"
-            className="inventory-card__icon"
-          />
-          {inventoryDetails.item_name}
-        </h1>
-        <button className="edit-cta">
-          <img
-            src={EditIcon}
-            alt="Sort Icon"
-            className="inventory-card__icon"
-          />
-        </button>
-        <hr className="inventory-card__divider"></hr>
-        <h4 className="inventory-card__heading--mobile">ITEM DESCRIPTION:</h4>
-        <p className="inventory-card__item-details">
-          {inventoryDetails.description}
-        </p>
-        <h4 className="inventory-card__heading--mobile">CATEGORY:</h4>
-        <p className="inventory-card__item-details">
-          {inventoryDetails.category}
-        </p>
-        <h4 className="inventory-card__heading--mobile">STATUS</h4>
-        <p
-          className={`inventory-card__status inventory-card__status--${statusClass}`}
-        >
-          {inventoryDetails.status.toUpperCase()}
-        </p>
+      {detailsLoaded ? (
+        <section className="card-container">
+          <h1 className="card-container__heading">
+            <img
+              src={BackIcon}
+              alt="Back Icon"
+              className="inventory-card__icon"
+            />
+            {inventoryDetails.item_name}
+          </h1>
+          <button className="edit-cta">
+            <img
+              src={EditIcon}
+              alt="Edit Icon"
+              className="inventory-card__icon"
+            />
+          </button>
+          <hr className="inventory-card__divider"></hr>
+          <h4 className="inventory-card__heading--mobile">ITEM DESCRIPTION:</h4>
+          <p className="inventory-card__item-details">
+            {inventoryDetails.description}
+          </p>
+          <h4 className="inventory-card__heading--mobile">CATEGORY:</h4>
+          <p className="inventory-card__item-details">
+            {inventoryDetails.category}
+          </p>
+          <h4 className="inventory-card__heading--mobile">STATUS</h4>
+          <p
+            className={`inventory-card__status inventory-card__status--${statusClass}`}
+          >
+            {inventoryDetails.status.toUpperCase()}
+          </p>
 
-        <h4 className="inventory-card__heading--mobile">QTY</h4>
-        <p className="inventory-card__item-details">
-          {inventoryDetails.quantity}
-        </p>
-        <h4 className="inventory-card__heading--mobile">WAREHOUSE</h4>
-        <p className="inventory-card__item-details">
-          {inventoryDetails.warehouse_name}
-        </p>
-      </section>
+          <h4 className="inventory-card__heading--mobile">QTY</h4>
+          <p className="inventory-card__item-details">
+            {inventoryDetails.quantity}
+          </p>
+          <h4 className="inventory-card__heading--mobile">WAREHOUSE</h4>
+          <p className="inventory-card__item-details">
+            {inventoryDetails.warehouse_name}
+          </p>
+        </section>
+      ) : (
+        "Loading"
+      )}
     </>
   );
 };
