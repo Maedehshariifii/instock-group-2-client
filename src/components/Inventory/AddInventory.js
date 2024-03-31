200bec817ccb031929028bf8f310b2e97bde01c0
Falak Zahra <falak.zahra16@gmail.com>
2024-03-30 18:19:40 -0600

    Apply form styles for tablet

--- src/components/Inventory/AddInventory.js
+++ src/components/Inventory/AddInventory.js
@@ -88,7 +88,7 @@ const AddInventory = () => {
     const addNewInventoryItem = async () => {
       if (submissionData && validSubmissionCount > 0) {
         try {
-          const resp = await axios.post(
+          await axios.post(
             "http://localhost:8080/api/inventories",
             submissionData
           );
@@ -116,170 +116,175 @@ const AddInventory = () => {
         </h2>
         <hr className="inventory-card__divider"></hr>
 
-        <form>
-          <h2 className="form-heading">Item Details</h2>
+        <form className="form">
+          <section className="form--left">
+            <h2 className="form-heading">Item Details</h2>
 
-          {/* name Input */}
-          <label className="form-label">Item Name</label>
-          <input
-            type="text"
-            name="item_name"
-            value={formData.item_name}
-            onChange={handleChange}
-            placeholder="Item Name"
-            className={`form-input ${
-              errors.item_name ? "form-input--error" : ""
-            }`}
-          />
-          {errors.item_name ? (
-            <p className="form-error">
-              <img
-                src={ErrorIcon}
-                alt="Error Icon"
-                className="form-error__icon"
-              />
-              This field is required
-            </p>
-          ) : null}
-          <br></br>
-
-          {/* Description Input */}
-          <label className="form-label">Description</label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            placeholder="Please enter a brief item description..."
-            className={`form-input--description ${
-              errors.description ? "form-input--error" : ""
-            }`}
-          />
-          {errors.description ? (
-            <p className="form-error">
-              <img
-                src={ErrorIcon}
-                alt="Error Icon"
-                className="form-error__icon"
-              />
-              This field is required
-            </p>
-          ) : null}
-          <br></br>
-
-          {/* Category selection */}
-          <label className="form-label">Category</label>
-          <select
-            name="category"
-            value={formData.category}
-            onChange={handleChange}
-            className={`form-input ${
-              errors.category ? "form-input--error" : ""
-            }`}
-          >
-            <option value="" disabled>
-              Please select
-            </option>
-            <option value="Electronics">Electronics</option>
-            <option value="Gear">Gear</option>
-            <option value="Apparel">Apparel</option>
-            <option value="Accessories">Accessories</option>
-            <option value="Health">Health</option>
-          </select>
-          {errors.category ? (
-            <p className="form-error">
-              <img
-                src={ErrorIcon}
-                alt="Error Icon"
-                className="form-error__icon"
-              />
-              This field is required
-            </p>
-          ) : null}
-          <hr className="inventory-card__divider"></hr>
-          <h2 className="form-heading">Item Availability</h2>
-
-          {/* status selection */}
-          <label className="form-label">Status</label>
-          <div className="form-input--status">
+            {/* name Input */}
+            <label className="form-label">Item Name</label>
             <input
-              type="radio"
-              value="In Stock"
-              name="status"
+              type="text"
+              name="item_name"
+              value={formData.item_name}
               onChange={handleChange}
-              checked={formData.status === "In Stock"}
+              placeholder="Item Name"
+              className={`form-input ${
+                errors.item_name ? "form-input--error" : ""
+              }`}
             />
-            In stock
-            <input
-              type="radio"
-              value="Out of Stock"
-              name="status"
-              onChange={handleChange}
-              checked={formData.status === "Out of Stock"}
-              className="status--right"
-            />
-            Out of Stock
-          </div>
+            {errors.item_name ? (
+              <p className="form-error">
+                <img
+                  src={ErrorIcon}
+                  alt="Error Icon"
+                  className="form-error__icon"
+                />
+                This field is required
+              </p>
+            ) : null}
+            <br></br>
 
-          {/* Conditionally render the Quantity field based on the status */}
-          {formData.status === "In Stock" && (
-            <div>
-              <label className="form-label">Quantity</label>
+            {/* Description Input */}
+            <label className="form-label">Description</label>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Please enter a brief item description..."
+              className={`form-input--description ${
+                errors.description ? "form-input--error" : ""
+              }`}
+            />
+            {errors.description ? (
+              <p className="form-error">
+                <img
+                  src={ErrorIcon}
+                  alt="Error Icon"
+                  className="form-error__icon"
+                />
+                This field is required
+              </p>
+            ) : null}
+            <br></br>
+
+            {/* Category selection */}
+            <label className="form-label">Category</label>
+            <select
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              className={`form-input ${
+                errors.category ? "form-input--error" : ""
+              }`}
+            >
+              <option value="" disabled>
+                Please select
+              </option>
+              <option value="Electronics">Electronics</option>
+              <option value="Gear">Gear</option>
+              <option value="Apparel">Apparel</option>
+              <option value="Accessories">Accessories</option>
+              <option value="Health">Health</option>
+            </select>
+            {errors.category ? (
+              <p className="form-error">
+                <img
+                  src={ErrorIcon}
+                  alt="Error Icon"
+                  className="form-error__icon"
+                />
+                This field is required
+              </p>
+            ) : null}
+            <hr className="inventory-card__divider--hidden"></hr>
+          </section>
+          <section className="form--right">
+            <h2 className="form-heading">Item Availability</h2>
+
+            {/* status selection */}
+            <label className="form-label">Status</label>
+            <div className="form-input--status">
               <input
-                name="quantity"
-                value={formData.quantity}
+                type="radio"
+                value="In Stock"
+                name="status"
                 onChange={handleChange}
-                placeholder="0"
-                className={`form-input ${
-                  errors.quantity ? "form-input--error" : ""
-                }`}
+                checked={formData.status === "In Stock"}
               />
-              {errors.quantity ? (
-                <p className="form-error">
-                  <img
-                    src={ErrorIcon}
-                    alt="Error Icon"
-                    className="form-error__icon"
-                  />
-                  Enter valid quantity
-                </p>
-              ) : null}
+              In stock
+              <input
+                type="radio"
+                value="Out of Stock"
+                name="status"
+                onChange={handleChange}
+                checked={formData.status === "Out of Stock"}
+                className="status--right"
+              />
+              Out of Stock
             </div>
-          )}
 
-          {/* Warehouse selection */}
-          <label className="form-label">Warehouse</label>
-          <select
-            name="warehouse_name"
-            value={formData.warehouse_name}
-            onChange={handleChange}
-            className={`form-input ${
-              errors.warehouse_name ? "form-input--error" : ""
-            }`}
-          >
-            <option value="" disabled>
-              Please select
-            </option>
-            <option value="Manhattan">Manhattan</option>
-            <option value="Washington">Washington</option>
-            <option value="Jersey">Jersey</option>
-            <option value="SF">SF</option>
-            <option value="Santa Monica">Santa Monica</option>
-            <option value="Seattle">Seattle</option>
-            <option value="Miami">Miami</option>
-            <option value="Boston">Boston</option>
-          </select>
-          {errors.warehouse_name ? (
-            <p className="form-error">
-              <img
-                src={ErrorIcon}
-                alt="Error Icon"
-                className="form-error__icon"
-              />
-              This field is required
-            </p>
-          ) : null}
-          <br></br>
+            {/* Conditionally render the Quantity field based on the status */}
+            {formData.status === "In Stock" && (
+              <div>
+                <label className="form-label">Quantity</label>
+                <input
+                  name="quantity"
+                  value={formData.quantity}
+                  onChange={handleChange}
+                  placeholder="0"
+                  className={`form-input ${
+                    errors.quantity ? "form-input--error" : ""
+                  }`}
+                />
+                {errors.quantity ? (
+                  <p className="form-error">
+                    <img
+                      src={ErrorIcon}
+                      alt="Error Icon"
+                      className="form-error__icon"
+                    />
+                    Enter valid quantity
+                  </p>
+                ) : null}
+              </div>
+            )}
+
+            {/* Warehouse selection */}
+            <label className="form-label">Warehouse</label>
+            <select
+              name="warehouse_name"
+              value={formData.warehouse_name}
+              onChange={handleChange}
+              className={`form-input ${
+                errors.warehouse_name ? "form-input--error" : ""
+              }`}
+            >
+              <option value="" disabled>
+                Please select
+              </option>
+              <option value="Manhattan">Manhattan</option>
+              <option value="Washington">Washington</option>
+              <option value="Jersey">Jersey</option>
+              <option value="SF">SF</option>
+              <option value="Santa Monica">Santa Monica</option>
+              <option value="Seattle">Seattle</option>
+              <option value="Miami">Miami</option>
+              <option value="Boston">Boston</option>
+            </select>
+            {errors.warehouse_name ? (
+              <p className="form-error">
+                <img
+                  src={ErrorIcon}
+                  alt="Error Icon"
+                  className="form-error__icon"
+                />
+                This field is required
+              </p>
+            ) : null}
+            <br></br>
+          </section>
         </form>
+
         <div className="cta">
           <button className="form-cta--cancel">Cancel</button>
           <button className="form-cta--add" onClick={handleSubmit}>
